34b14db12d3f5173fe239e6715db95633f2ada33
unknown <karamadel.36@gmail.com>
2026-03-25 18:33:54 +0200

    add dark mode JavaScript

---
+++ day-02-dark-mode/docs/main.js
@@ -0,0 +1,8 @@
+const darkMode = document.getElementById("moon");
+const lightMode = document.getElementById("sun");
+darkMode.addEventListener("click", () =>
+  document.body.classList.toggle("dark"),
+);
+lightMode.addEventListener("click", () =>
+  document.body.classList.toggle("dark"),
+);
