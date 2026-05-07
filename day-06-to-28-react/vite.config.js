6e14f083df570647d704b0e220602af5146649ad
unknown <karamadel.36@gmail.com>
2026-05-07 14:31:24 +0300

    fixing live repos issue

--- day-06-to-28-react/vite.config.js
+++ day-06-to-28-react/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/frontend-28-days/day-06-to-28-react/",
+  base: "./",
   plugins: [react(), tailwindcss()],
 });
